3b8f3846cacbd439e3a6dbb16f13a9272441a563
jin101501 <117827685+jin101501@users.noreply.github.com>
2023-05-10 16:50:32 +0900

    Add project files.

---
+++ AspNetCoreStudy/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AspNetCoreStudy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+namespace AspNetCoreStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //var firstUser = new User();
+            //firstUser.UserNo = 1;
+            //firstUser.UserName = "홍길동";
+
+            var firstUser = new User
+            {
+                UserNo = 1,
+                UserName="홍길동"
+            };
+            return View(firstUser);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCoreStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
